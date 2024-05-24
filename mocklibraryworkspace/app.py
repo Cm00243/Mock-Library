a582916d51fa851843a772856df774f8275ecb6c
Connor McIntire <connorjmcintire@gmail.com>
2024-05-24 02:09:24 -0400

    Decorating
    I formatted each of the pages to the color scheme I wanted and finished the featured books section. The books page and the home page are complete as of right now.

--- mocklibraryworkspace/app.py
+++ mocklibraryworkspace/app.py
@@ -1,7 +1,61 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, redirect, url_for, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import csv
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+# User model
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/books')
+def books_page():
+    books = read_csv()
+    return render_template('books.html', books=books)
+
+@app.route('/contact')
+def contact_page():
+    return render_template('contact.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for('profile'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html')
+
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', name=current_user.username)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 def read_csv(file_path='books.csv'):
     books = []
@@ -15,20 +69,6 @@ def read_csv(file_path='books.csv'):
                 "publishing_date": row['publishing_date']
             })
     return books
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/books')
-def books_page():
-    books = read_csv()
-    return render_template('books.html', books=books)
-
-@app.route('/api/books')
-def get_books():
-    books = read_csv()
-    return jsonify(books)
 
 if __name__ == '__main__':
     app.run(debug=True)
